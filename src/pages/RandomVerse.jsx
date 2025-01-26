05aed4e1c4c6f0c2fc9262c744c64bce9d366d65
Edzer Dionido <jeddionido26@gmail.com>
2025-01-27 07:37:49 +0800

    add Random Verse Component and add functionality

--- src/pages/RandomVerse.jsx
+++ src/pages/RandomVerse.jsx
@@ -1,7 +1,29 @@
+import styled from "styled-components";
+import Row from "../ui/Row";
+import Box from "../ui/Box";
+import RandomVerses from "../features/random-verses/RandomVerses";
+import { Button } from "@mui/material";
+import { useRandomVerse } from "../features/random-verses/useRandomVerse";
+
+const StyledRow = styled(Row)`
+    flex-direction: column;
+    margin: 2rem auto;
+`
+
+
 export default function RandomVerse() {
+    const {refetch} = useRandomVerse();
+    function handleClick(){
+        refetch();
+    }
+    
     return (
-        <div>
-            
-        </div>
+        <StyledRow>
+            <h1>Random Bible Verses</h1>
+            <Box>
+                <RandomVerses/>
+            </Box>
+            <Button variant="outlined" onClick={handleClick}>Generate New Verse</Button>
+        </StyledRow>
     )
 }
