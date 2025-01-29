2fec50b0791497590ce58fafea00559384074b0b
Edzer Dionido <jeddionido26@gmail.com>
2025-01-29 19:23:17 +0800

    collected chapters from the bible param and put them to button to get the Bible Chapter

---
+++ src/features/bible/Chapters.jsx
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+import { useGetChapters } from "./useGetChapters";
+import { Link } from "react-router";
+import Spinner from "../../ui/Spinner";
+
+export default function Chapters({ book }) {
+  const { loadingChapters, bookChapters, error } = useGetChapters(book);
+
+  if (loadingChapters) return <Spinner />;
+
+  const { chapters } = bookChapters;
+
+  return (
+    <div>
+      {chapters.map((chapter) => (
+        <Button
+          sx={{ m: "1rem" }}
+          variant="contained"
+          key={`${chapter.book_id} ${chapter.chapter}`}
+          component={Link} // Use the Button's component prop
+          to={`/bible/${book}/${chapter.chapter}`} // Pass the "to" prop for navigation
+        >
+          {chapter.chapter}
+        </Button>
+      ))}
+    </div>
+  );
+}
