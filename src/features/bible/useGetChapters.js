4a8bdf83968b9161ed7ea10373d24e31a7448444
Edzer Dionido <jeddionido26@gmail.com>
2025-01-30 16:34:26 +0800

    add caching to dataSet

--- src/features/bible/useGetChapters.js
+++ src/features/bible/useGetChapters.js
@@ -2,12 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { getBibleChapters } from "../../api/apiBible";
 
 export const useGetChapters = (book) => {
-    console.log(book);
-    const {isLoading: loadingChapters, data: bookChapters, error} = useQuery({
-        queryKey: ['bible-chapter', book],
-        queryFn: () => getBibleChapters(book),
-        refetchOnWindowFocus: false
-    });
+  const {
+    isLoading: loadingChapters,
+    data: bookChapters,
+    error,
+  } = useQuery({
+    queryKey: ["bible-chapter", book],
+    queryFn: () => getBibleChapters(book),
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 5,
+    cacheTime: 1000 * 60 * 30,
+    enabled: !!book,
+  });
 
-    return {loadingChapters, bookChapters, error}
-}
+  return { loadingChapters, bookChapters, error };
+};
