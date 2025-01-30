aded08e942777fd118d632747237e2e560c6df00
Edzer Dionido <jeddionido26@gmail.com>
2025-01-30 16:00:46 +0800

    Add BibleVerse component inside

--- src/pages/Verse.jsx
+++ src/pages/Verse.jsx
@@ -1,7 +1,9 @@
+import BibleVerse from "../features/bible/verses/BibleVerse";
+
 export default function Verse() {
     return (
         <div>
-            
+            <BibleVerse/>
         </div>
     )
 }
