b252061b322679809ac09bc9ad4b3f0751279018
Edzer Dionido <jeddionido26@gmail.com>
2025-01-30 16:32:56 +0800

    add bible verse

---
+++ src/features/bible/verses/BibleVerse.jsx
@@ -0,0 +1,72 @@
+import { useParams } from "react-router";
+import { useGetVerses } from "./useGetVerses";
+import Spinner from "../../../ui/Spinner";
+import PageNotFound from "../../../pages/PageNotFound";
+import styled from "styled-components";
+
+// Styled Components
+const VerseContainer = styled.div`
+  display: inline-block;
+  width: 80%;
+  max-width: 900px; // Prevents stretching too wide on large screens
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const VerseHeading = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  color: #333;
+`;
+const VerseNumber = styled.span`
+  font-weight: bold;
+  color: #007bff;
+  margin-right: 0.25rem;
+`;
+const VerseText = styled.span`
+  font-size: 1.2rem;
+  line-height: 1.6;
+  text-align: left;
+  color: #555;
+`;
+
+const VerseWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  min-height: 100vh;
+  background-color: #f4f4f4;
+`;
+
+export default function BibleVerse() {
+  const { book, chapter, verse } = useParams();
+
+  const { loadVerses, bibleVerses, error } = useGetVerses(book, chapter, verse);
+  
+  if (loadVerses) return <Spinner />;
+  if (error) return <PageNotFound />;
+
+  const verses = bibleVerses?.verses || [];
+
+  return (
+    <VerseWrapper>
+      <VerseContainer>
+        <VerseHeading>
+          {verses[0]?.book_name} {chapter}:{verse}
+        </VerseHeading>
+        <VerseText>
+          {verses.map((verses) => (
+            <>
+            <VerseNumber key={verses.verse}>{verses.verse}</VerseNumber>
+            <VerseText>{verses.text}</VerseText>
+            </>
+          ))}
+        </VerseText>
+      </VerseContainer>
+    </VerseWrapper>
+  );
+}
---
+++ src/features/bible/verses/useGetVerses.jsx
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { getBibleVerses } from "../../../api/apiBible";
+
+export const useGetVerses = (book, chapter, verses)=>{
+    const {isLoading: loadVerses, data: bibleVerses, error} = useQuery({
+        queryKey: ['bibleVerse', book, chapter, verses],
+        queryFn: () => getBibleVerses(book, chapter, verses),
+        refetchOnWindowFocus: false,
+    });
+
+    return {loadVerses, bibleVerses, error};
+}
