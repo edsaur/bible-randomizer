1ab0d9a15b7aaad5f7199b591678931644adbb0d
Edzer Dionido <jeddionido26@gmail.com>
2025-02-21 16:21:28 +0800

    remove staletime and refocusonwindowchange

--- src/features/bible/verses/useGetVerses.jsx
+++ src/features/bible/verses/useGetVerses.jsx
@@ -5,8 +5,6 @@ export const useGetVerses = (book, chapter, verses)=>{
     const {isLoading: loadVerses, data: bibleVerses, error} = useQuery({
         queryKey: ['bibleVerse', book, chapter, verses],
         queryFn: () => getBibleVerses(book, chapter, verses),
-        refetchOnWindowFocus: false,
-        staleTime: 1000 * 10 * 5
     });
 
     return {loadVerses, bibleVerses, error};
